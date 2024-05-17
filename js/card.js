d00eb0cdca24ca73cac88555a9e89ad6f78bc210
JaumeUNI <jcasamitjana@campus.eug.es>
2024-05-17 18:20:05 +0200

    img cards

--- js/card.js
+++ js/card.js
@@ -7,17 +7,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const gallery = document.getElementById('gallery');
 
     const cards = [
-        { img: "https://via.placeholder.com/500", title: "Ciudad en Primavera", description: "Una ciudad floreciendo en primavera.", season: "primavera", zone: "ciudad" },
-        { img: "https://via.placeholder.com/500", title: "Playa en Verano", description: "Sol brillante en la playa durante el verano.", season: "verano", zone: "playa" },
-        { img: "https://via.placeholder.com/500", title: "Montaña en Otoño", description: "Hojas otoñales en la montaña.", season: "otoño", zone: "montaña" },
-        { img: "https://via.placeholder.com/500", title: "Ciudad en Invierno", description: "Cubierta de nieve en la ciudad.", season: "invierno", zone: "ciudad" }
-
-        // ...
+        { img: "./fotos/veranoPlaya_foto1.jpg", title: "Playa en Verano", description: "Una playa pardisíaca en Australia durante verano.", season: "verano", zone: "playa" },
+        { img: "./fotos/veranoCiudad_foto2.jpg", title: "Ciudad en Verano", description: "Una foto donde se ve la vista de la ciudad de Barcelona en verano.", season: "verano", zone: "ciudad" },
+        { img: "./fotos/veranoCiudad_foto3.jpg", title: "Ciudad en Verano", description: "Una foto donde se ve los edificios de una parte de la ciudad de Nueva York en verano.", season: "verano", zone: "ciudad" },
+        { img: "./fotos/veranoCiudad_foto4.jpg", title: "Ciudad en Verano", description: "Una foto donde se ve la vista desde arriba de la ciudad de Río de Janeiro en verano.", season: "verano", zone: "ciudad" },
+        { img: "./fotos/veranoPlaya_foto5.jpg", title: "Playa en Verano", description: "Una playa pardisíaca de arena blanca con unas montañas en el fondo esto es en Indonesia durante verano.", season: "verano", zone: "playa" },
+        { img: "./fotos/veranoPlaya_foto6.jpg", title: "Playa en Verano", description: "Una playa de aguas cristalinas en la isla de Mallorca durante verano.", season: "verano", zone: "playa" },
+        { img: "./fotos/inviernoMonta_foto7.jpg", title: "Montaña en Invierno", description: "Una foto donde se ve la montaña Matterhorn nevada durante el invierno.", season: "invierno", zone: "montaña" },
+        { img: "./fotos/inviernoCiudad_foto8.jpg", title: "Ciudad en Invierno", description: "Una foto donde se ve la torre Eiffel nevada durante el invierno en la ciudad de París .", season: "invierno", zone: "ciudad" },
+        { img: "./fotos/inviernoCiudad_foto9.jpg", title: "Ciudad en Invierno", description: "Una foto donde se ve la ciudad de Saint-Moritz durante una noche de invierno completamente iluminada .", season: "invierno", zone: "ciudad" },
+        { img: "./fotos/otonoMonta_foto10.jpg", title: "Montaña en Otoño", description: "Una vista del parque natural de Somiedo en Asturias cubierto por árboles.", season: "otoño", zone: "montaña" },
+        { img: "./fotos/otonoCiudad_foto11.jpg", title: "Ciudad en Otoño", description: "Una foto de la ciudad de París durante el otoño donde se ve a través de unos árboles la torre Eiffel.", season: "otoño", zone: "ciudad" }  
     ];
 
     function filterCards() {
-        const season = seasonFilter.value;
-        const zone = zoneFilter.value;
+        const season = seasonFilter.value.toLowerCase();
+        const zone = zoneFilter.value.toLowerCase();
 
         const filteredCards = cards.filter(card =>
             (card.season === season || season === "") &&
@@ -45,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Configura el filtro de temporada desde la URL si existe
     if (seasonFilterFromUrl) {
-        seasonFilter.value = seasonFilterFromUrl;
+        seasonFilter.value = seasonFilterFromUrl.toLowerCase();
     }
 
     // Event listeners para los filtros
